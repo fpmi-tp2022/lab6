09343c171530731cbca0e65e8b6f0c21dcf459cd
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2022-06-03 17:52:46 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,12 @@
+let a = Double(readLine()!);
+let b = Double(readLine()!);
+let c = Double(readLine()!);
+let d = Double(readLine()!);
+
+if ((a! > c! && b! > d!) || (a! > d! && b! > c!)){
+  print("mojno")
+}
+else {
+  print("nelzya")
+}
+
