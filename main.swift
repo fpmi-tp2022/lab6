e3dce3dab0094aa37b422cb4f38fb515b408ba32
RETUR07 <75529203+RETUR07@users.noreply.github.com>
2022-06-03 18:14:01 +0000

    task5 init

--- main.swift
+++ main.swift
@@ -1,12 +1,25 @@
-let a = Double(readLine()!);
-let b = Double(readLine()!);
-let c = Double(readLine()!);
-let d = Double(readLine()!);
+import Foundation
+import Glibc
 
-if ((a! > c! && b! > d!) || (a! > d! && b! > c!)){
-  print("mojno")
-}
-else {
-  print("nelzya")
-}
+var flights : [String : Int] // init empty
 
+var airports : [String : String] = ["a" : "b", "c" : "d"]
+
+print(airports.count) // count
+
+print("a")
+print(airports["a"]!) // print key and value
+
+for (name, city) in airports{
+    print("\(name) : \(city)")
+} // print all elements
+
+airports["e"] = "u" // add
+airports["a"] = "t" // update
+
+airports.removeValue(forKey : "c")
+print()
+
+for (name, city) in airports{
+    print("\(name) : \(city)")
+}
